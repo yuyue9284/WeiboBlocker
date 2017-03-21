e249d1e70b386a7b98cef1034c7ad63c7557c427
Yue Yu <yuyue9284@outlook.com>
2017-03-20 23:08:39 -0700

    缓存屏蔽列表

--- weibo.js
+++ weibo.js
@@ -6,25 +6,59 @@
 // @author       yuyue9284@gmail.com
 // @include      http://weibo.com*
 // @grant        none
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var action = function(){
-        var block = ["BBC","lily"];
-        var lst = $(".WB_text").map(function(){return $(this);});
+
+    // gn_header clearfix
+    var block = [];
+    var init = function() {
+        if (localStorage.block !== undefined) {
+            block = JSON.parse(localStorage.block);
+        } else {
+            block = ['tf', 'TF', 'Tf', '王源', '易烊千玺', '王俊凯'];
+            localStorage.block = JSON.stringify(block);
+            console.log(localStorage.block);
+        }
+        $(".gn_header.clearfix").off('click', add_word).on('click', add_word);
+    };
+
+    function add_word() {
+        var word = prompt('请输入');
+        switch (word) {
+            case "clear":
+                localStorage.removeItem('block');
+                block = [];
+                break;
+            case "show":
+                alert(block);
+                break;
+            default:
+                block = block.concat(word);
+                localStorage.block = block;
+        }
+        action();
+    }
+
+    $(document).ready(init);
+
+    var action = function() {
+        var lst = $(".WB_cardwrap.WB_feed_type.S_bg2").map(function() {
+            return $(this);
+        });
         for (var i = lst.length - 1; i >= 0; i--) {
             for (var j = block.length - 1; j >= 0; j--) {
                 var content = lst[i].text();
-                if (content.includes(block[j])){
-                    lst[i].parents().eq(2).css("display","none");
-                    console.log(content);
+                if (content.includes(block[j])) {
+                    lst[i].css("display", "none");
+                    // console.log(content);
                     break;
                 }
             }
         }
     };
-    waitForKeyElements (".WB_text", action);
+    waitForKeyElements(".WB_cardwrap.WB_feed_type.S_bg2", action);
 })();
