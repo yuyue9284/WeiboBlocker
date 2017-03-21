4b405dd7cbe02850838e1ed0b6ac9cdfbc10aa57
Yue Yu <yuyue9284@outlook.com>
2017-03-21 13:57:17 -0700

    change ver#

--- weibo.js
+++ weibo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WeiboBlocker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       yuyue9284@gmail.com
 // @include      http://weibo.com*
